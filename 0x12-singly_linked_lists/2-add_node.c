b56073d7bd0e238615ac653456cf809819adfa03
Awwal <awwalolasunkanmi2005@gmail.com>
2022-11-25 00:55:43 -0800

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,8 +8,9 @@
  * @str: The string to be added to the list_t list.
  *
  * Return: If the function fails - NULL.
- *		Otherwise - the address of the new element.
+ *	Otherwise - the address of the new element.
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	char *dup;
